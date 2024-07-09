07173ac4df38e40e57960258480e2ff7d25b5e3f
jaimini26 <jaiminimistry25@gmail.com>
2024-07-09 23:22:12 +0530

    design update of tale and column add

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,50 +6,54 @@ import DatePicker from "react-datepicker";
 
 function Home() {
   const [stockDetails, setStockDetails] = useState(data.default);
-  const [startDate, setStartDate] = useState(new Date());
   const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
+    { value: "BANKNIFTY ", label: "BANKNIFTY " },
+    { value: "NIFTY", label: "NIFTY" },
+    { value: "FINNIFTY", label: "FINNIFTY" },
+    { value: "SENSEX", label: "SENSEX" },
+    { value: "BANKEX", label: "BANKEX" },
   ];
-  const header = ["Sr. No.", "Trading Symbol", "LTP", "Trading Symbol", "LTP"];
+  const Dateoptions = [
+    { value: "10 july", label: "10 july" },
+    { value: "09 july", label: "09 july" },
+    { value: "08 july", label: "08 july" },
+  ];
+  const header = ["Sr. No.", "Trading Symbol", "LTP", "Trading Symbol", "LTP", "Straddle"];
   return (
     <>
       <div>
-        <div className="tbl-top">Option Chain</div>
+        <div className="tbl-top">
+          <p className="TitleTab">Option Chain</p>
+        </div>
       </div>
       <div className="filter-search-bar d-flex justify-content-between mb-2">
         <Select options={options} className="select-bar" />
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          className="inp"
-        />
-        <p>
+        <Select options={Dateoptions} className="select-bar" />
+        <p className="mb-0">
           PCR:{" "}
           <span>
             <strong>3.15</strong>
           </span>
         </p>
-        <p>
+        <p className="mb-0">
           Max pain:{" "}
           <span>
             <strong>2.45</strong>
           </span>
         </p>
-        <p>
+        <p className="mb-0">
           IVP:{" "}
           <span>
             <strong>78.45</strong>
           </span>
         </p>
-        <p>
+        <p className="mb-0">
           Synthetic Future:{" "}
           <span>
             <strong>3.85</strong>
           </span>
         </p>
-        <p>
+        <p className="mb-0">
           {" "}
           ATM Straddle:{" "}
           <span>
@@ -57,7 +61,8 @@ function Home() {
           </span>
         </p>
       </div>
-      <Table striped bordered hover>
+      <div className="ThreadTable style-2-Scrolbar">
+      <Table  className="Thread_Table"  striped hover>
         <thead>
           <tr>
             {header.map((name) => (
@@ -73,10 +78,12 @@ function Home() {
               <td>{data?.CEltp}</td>
               <td>{data?.PEtradingSymbol}</td>
               <td>{data?.PEltp}</td>
+              <td>12345</td>
             </tr>
           ))}
         </tbody>
       </Table>
+      </div>
     </>
   );
 }
